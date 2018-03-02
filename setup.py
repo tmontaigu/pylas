be739286dce43ecae178045f6b624a6411f9d616
Jälv <thomas.montaigu@laposte.net>
2018-03-02 17:57:24 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pylas',
+    version='0.1',
+    description='Las/Laz in python',
+    url='https://github.com/tmontaigu/pylas',
+    author='Thomas Montaigu',
+    # license='MIT',
+    packages=['pylas'],
+    zip_safe=False
+)
