6d6458883f7005edc7f6b4ef012c2473d22ca30b
Jälv <thomas.montaigu@laposte.net>
2018-04-12 18:43:00 +0200

    add install requires

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='Las/Laz in python',
     url='https://github.com/tmontaigu/pylas',
     author='Thomas Montaigu',
+    python_requires='>=3',
     install_requires=['numpy'],
     license='BSD 3-Clause',
     packages=find_packages(exclude=('pylastests',)),
