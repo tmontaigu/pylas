2a29494864ba22e91f178c58d66af7cb91521aef
Jälv <thomas.montaigu@laposte.net>
2018-06-14 14:46:30 +0200

    move version in pylas's top level __init__

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+import pylas
 
 with open("README.rst") as f:
     readme = f.read()
 
 setup(
     name="pylas",
-    version="0.1.3",
+    version=pylas.__version__,
     description="Las/Laz reading and writing in python",
     long_description=readme,
     url="https://github.com/tmontaigu/pylas",
