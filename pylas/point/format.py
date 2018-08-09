7671409f3f03e941d3b16c3f0213a98b87d23c60
tmontaigu <thomas.montaigu@laposte.net>
2018-08-09 10:57:31 +0200

    define the point format class
    Goal Have a higher level class to query information about a particular point format

---
+++ pylas/point/format.py
@@ -0,0 +1,61 @@
+from . import dims
+from .. import errors
+
+
+class PointFormat:
+    def __init__(self, point_format_id, extra_dims=None):
+        self.id = point_format_id
+        self.extra_dims = extra_dims
+
+    @property
+    def dimension_names(self):
+        return self._access_dict(dims.ALL_POINT_FORMATS_DIMENSIONS, self.id)
+
+    @property
+    def dtype(self):
+        dtype = self._access_dict(dims.ALL_POINT_FORMATS_DTYPE, self.id)
+        dtype = self._dtype_add_extra_dims(dtype)
+        return dtype
+
+    @property
+    def unpacked_dtype(self):
+        dtype = self._access_dict(dims.UNPACKED_POINT_FORMATS_DTYPES, self.id)
+        dtype = self._dtype_add_extra_dims(dtype)
+        return dtype
+
+    @property
+    def composed_fields(self):
+        return self._access_dict(dims.COMPOSED_FIELDS, self.id)
+
+    @property
+    def sub_fields(self):
+        sub_fields_dict = {}
+        for composed_dim_name, sub_fields in self.composed_fields.items():
+            for sub_field in sub_fields:
+                sub_fields_dict[sub_field.name] = (composed_dim_name, sub_field)
+        return sub_fields_dict
+
+    @property
+    def has_waveform_packet(self):
+        dimensions = set(self.dimension_names)
+        return all(name in dimensions for name in dims.WAVEFORM_FIELDS_NAMES)
+
+    def _access_dict(self, d, key):
+        try:
+            return d[key]
+        except KeyError as e:
+            raise errors.PointFormatNotSupported(e)
+
+    def _dtype_add_extra_dims(self, dtype):
+        if self.extra_dims is not None:
+            dtype = dims.dtype_append(dtype, self.extra_dims)
+        return dtype
+
+    def __int__(self):
+        return self.id
+
+    def __str__(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return "<PointFormat({})>".format(self.id)
